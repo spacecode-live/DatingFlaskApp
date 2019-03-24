5e5a671e37985b4da633a288566ad787f3f44f21
SelinaMangaroo <selinamangaroo@gmail.com>
2019-03-24 19:29:39 -0400

    sprint2 doc
    documentation for sprint2

--- dating/__init__.py
+++ dating/__init__.py
@@ -3,11 +3,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_migrate import Migrate
 from flask_login import LoginManager
-from dating.config import Config
+from dating.config import Config   #from config module, import config class
 
 app = Flask(__name__)
 
-app.config.from_object(Config)
+app.config.from_object(Config) #tells flask to use and apply the config file
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
